67963c63909a72ccf9203a7c8650923196ab5ecd
Mimameid <ibrahimkeser@outlook.de>
2021-02-14 11:57:39 +0100

    Added a basis for theming support for the application.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import { Grid } from '@material-ui/core';
 import NavigationDrawer from './components/NavigationDrawer/NavigationDrawer';
 import UpdateDeliveryZone from './components/UpdateDeliveryZones/UpdateDeliveryZone';
 
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
+import theme from './theme/theme';
+
 import store from './store/store';
 
 function App() {
   return (
-    <Provider store={store}>
-      <CssBaseline>
-        <NavigationDrawer />
-        <Grid container direction="column">
-          <UpdateDeliveryZone />
-        </Grid>
-      </CssBaseline>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <CssBaseline>
+          <NavigationDrawer />
+          <Grid container direction="column">
+            <UpdateDeliveryZone />
+          </Grid>
+        </CssBaseline>
+      </Provider>
+    </ThemeProvider>
   );
 }
 
